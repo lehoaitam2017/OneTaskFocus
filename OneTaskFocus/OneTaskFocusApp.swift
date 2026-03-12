abacc118dc29589f5c5841fb1e3d3d23501e63ef
tatale <tam.le@oracle.com>
2026-03-12 15:49:56 -0700

    Initial Commit

---
+++ OneTaskFocus/OneTaskFocusApp.swift
@@ -0,0 +1,32 @@
+//
+//  OneTaskFocusApp.swift
+//  OneTaskFocus
+//
+//  Created by Tam Le on 3/12/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OneTaskFocusApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
